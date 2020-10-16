1a90f5d288688bfaf05506a18d995b46c34fe86c
GodzillaJim <47969657+GodzillaJim@users.noreply.github.com>
2020-10-16 20:11:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+
+export default class App extends React.Component{
+  render(){
+    return "Hello World!"
+  }
+}
+//Home is login, authenticated side is all questions
---
+++ src/everything.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+Switch, Route, Link
+} from 'react-router-dom'
+
+class Everything extends React.Component{
+  render(){
+    return <div>Contains Everything</div>
+  }
+}
+export default Everything
